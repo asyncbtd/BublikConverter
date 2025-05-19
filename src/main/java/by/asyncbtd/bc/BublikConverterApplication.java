1155df6f81c03a5968bf4b112122f3df51aa513f
Timofey Brukhanchik <asyncbtd@gmail.com>
2025-05-19 18:23:52 +0300

    init project

---
+++ src/main/java/by/asyncbtd/bc/BublikConverterApplication.java
@@ -0,0 +1,13 @@
+package by.asyncbtd.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BublikConverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BublikConverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/asyncbtd/bc/BublikConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package by.asyncbtd.bc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BublikConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
